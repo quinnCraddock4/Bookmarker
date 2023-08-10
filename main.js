368eecfa41992c413b3baf892f3b2a6a8c575ecb
quinnCraddock4 <quinncraddock@DESKTOP-UI3TT7Q.localdomain>
2023-08-09 20:19:39 -0500

    basic ui and return all bookmarks

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log(chrome.bookmarks)
+function makeIndent(indentLength) {
+    return ".".repeat(indentLength);
+  }
+  
+  function logItems(bookmarkItem, indent) {
+    if (bookmarkItem.url) {
+      console.log(makeIndent(indent) + bookmarkItem.title);
+    } else {
+      console.log(`${makeIndent(indent)}Folder`);
+      indent++;
+    }
+    if (bookmarkItem.children) {
+      for (const child of bookmarkItem.children) {
+        logItems(child, indent);
+      }
+    }
+    indent--;
+  }
+  
+  function logTree(bookmarkItems) {
+    console.log(bookmarkItems)
+    logItems(bookmarkItems[0], 0);
+  }
+  
+  function onRejected(error) {
+    console.log(`An error: ${error}`);
+  }
+  
+  let gettingTree = chrome.bookmarks.getTree();
+  gettingTree.then(logTree, onRejected);
